fa1920b270b8bca80c8705511eef3458540ab33a
salma-jahan-sahara <salmajahansahara@gmail.com>
2023-09-20 02:04:05 +0600

    structured

--- New folder/menu.js
+++
@@ -1,6 +0,0 @@
-// const price__change
-$(".price__change").click(function(){
-    // alert($(this).attr("data-price"));
-    var price = $(this).attr("data-price");
-    $("#food-price_1").html(price);
-});
---
+++ js/timer.js
@@ -0,0 +1,26 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline__format span");
+console.log(items);
